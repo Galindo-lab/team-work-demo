3f2641a6308993b8de7a74c19579c7d1fe7d4a05
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-06-22 11:58:06 -0700

    objetivos agregados

---
+++ teamworkdemo/teamwork/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# - personas que pueden ser integrantes o evaluadores 
+
+# - grupos a lo que pertenecen los miembros 
+
+# - formularios realizados
---
+++ teamworkdemo/teamwork/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# dashboard 
+
+# crear grupos 
+
+# elimiar grupos 
+
+# agregar a un grupo
+
+# eliminar de un grupo 
+
+# hacer formulario de belbin 
