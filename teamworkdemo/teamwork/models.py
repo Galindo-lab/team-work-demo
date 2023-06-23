1eb5da38cd8f32fae3d6d98755492b149544e542
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-06-22 22:15:12 -0700

    modelos funcionan

--- teamworkdemo/teamwork/models.py
+++ teamworkdemo/teamwork/models.py
@@ -1,7 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# - personas que pueden ser integrantes o evaluadores 
 
-# - grupos a lo que pertenecen los miembros 
+class Member(models.Model):
+    """
+    personas que pueden crear formularios y resolverlos
+    """
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
 
-# - formularios realizados
+
+class Group(models.Model):
+    """
+    grupos a lo que pertenecen los miembros
+    """
+    group_name = models.CharField(
+        max_length=200
+    )
+
+    group_creator = models.ForeignKey(
+        User,
+        related_name='author',
+        on_delete=models.CASCADE
+    )
+
+    
+
+class Integrante(models.Model):
+    """
+    Relaciona los miembros con los grupos 
+    """
+
+    member = models.ForeignKey(
+        User, 
+        related_name='part_of',
+        on_delete=models.CASCADE
+    )
+
+    group = models.ForeignKey(
+        Group, 
+        related_name='members',
+        on_delete=models.CASCADE
+    ) 
+
+
+
+class BelbinUserProfile(models.Model):
+    """
+    formularios realizados
+    """
+
+    auth_user_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    group_id = models.ForeignKey(
+        Group, 
+        related_name='emited_by',
+        on_delete=models.CASCADE
+    )
+
+    timestamp = models.DateTimeField(
+        default=timezone.now
+    )
+
+    # perfiles de belbin
+    resource_investigator = models.IntegerField()
+    team_worker = models.IntegerField()
+    coordinator = models.IntegerField()
+    plant = models.IntegerField()
+    monitor_evaluator = models.IntegerField()
+    specialist = models.IntegerField()
+    shaper = models.IntegerField()
+    implementer = models.IntegerField()
+    completer_finisher = models.IntegerField()
+    
+    class Meta:
+        ordering = ['-timestamp']
